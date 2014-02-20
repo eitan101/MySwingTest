467ab01b2ac789137689b3fc3a4c3aff07c50556
eitan101 <eitan@paralleluniverse.co>
2014-02-20 15:18:06 +0200

    initial

---
+++ src/main/java/swingtest/ButtonFg.java
@@ -0,0 +1,52 @@
+package swingtest;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.*;
+import org.pushingpixels.trident.Timeline;
+import org.pushingpixels.trident.ease.TimelineEase;
+
+public class ButtonFg extends JFrame {
+
+    public ButtonFg() {
+
+        JButton button = new JButton("sample");
+        final Color beginColor = new Color(100,100,100);
+        final Color endColor = new Color(30, 30, 30);
+        button.setForeground(Color.white);
+        button.setOpaque(true);
+        button.setFocusPainted(false);
+        button.setBorder(BorderFactory.createEmptyBorder(10 , 20, 10, 20));
+        button.setBackground(beginColor);
+        this.setLayout(new FlowLayout());
+        this.add(button);
+        final Timeline rolloverTimeline = new Timeline(button);
+        rolloverTimeline.addPropertyToInterpolate("background", beginColor, endColor);
+        rolloverTimeline.setDuration(200);
+        button.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                rolloverTimeline.play();
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                rolloverTimeline.playReverse();
+            }
+        });
+        this.setSize(400, 200);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ButtonFg().setVisible(true);
+            }
+        });
+    }
+}
